5d1b432cbc24b28afc5c61bdceac9c8697e24647
Marcelina <marcelinasuszczyk@gmail.com>
2022-01-16 15:38:46 +0100

    layout

--- app.py
+++ app.py
@@ -12,41 +12,53 @@ from dash_bootstrap_templates import load_figure_template
 
 load_figure_template("solar")
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.SOLAR])
+app.title = "String Learning GA"
 
 SELECTION_RATE = 0.1
 MUTATION_RATE = 0.01
 ALPHABET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789. "
 
-app.layout = html.Div([
-    html.H1("String Learning using Genetic Algorithm"),
-    html.Div([
-        html.H4("Write your target string: "),
-        dcc.Input(id='target', type='text')
-    ]),
-    html.Div([
-        html.H4("Choose the size of the population: "),
-        dcc.Slider(
-            id='population_size',
-            min=20,
-            max=500,
-            step=10,
-            value=250,
-            tooltip={"placement": "bottom", "always_visible": True})
-    ]),
-    html.Div([
-        html.H4("Choose maximal number of iterations: "),
-        dcc.Slider(
-            id='iterations',
-            min=20,
-            max=500,
-            step=10,
-            value=250,
-            tooltip={"placement": "bottom", "always_visible": True}
-        )
-    ]),
-    html.Button('Submit', id='submit_button', n_clicks=0),
-    html.Div(id='output_container', children=[]),
-    dcc.Graph(id="evaluation", figure={})
+h_style={'text-align': 'center', 'padding': 10}
+
+
+app.layout = html.Div(style={'font-family': 'Noto'}, children=[
+    dbc.Row([
+        dbc.Col([
+            html.H1(["String Learning", html.Br(), "using", html.Br(), "Genetic Algorithm"], style=h_style),
+            dbc.Row([
+                html.H4("Write your target text: ", style=h_style),
+                dcc.Input(id='target',
+                          type='text',
+                          placeholder='your text',
+                          style={'text-align': 'center'},
+                          maxLength=50)
+            ]),
+            html.Br(),
+            html.H4("Choose the size of the population: ", style=h_style),
+            dcc.Slider(
+                id='population_size',
+                min=20,
+                max=500,
+                step=10,
+                value=250,
+                tooltip={"placement": "bottom", "always_visible": True}),
+            html.Br(),
+            html.H4("Choose maximal number of iterations: ", style=h_style),
+            dcc.Slider(
+                id='iterations',
+                min=20,
+                max=500,
+                step=10,
+                value=250,
+                tooltip={"placement": "bottom", "always_visible": True}),
+            html.Br(),
+            dbc.Row(html.Button('Submit', id='submit_button', n_clicks=0))
+        ], width=4),
+        dbc.Col([
+            html.Div(id='output_container', children=[]),
+            dcc.Graph(id="evaluation", figure={})
+        ], width=8)
+    ])
 ])
 
 
